c95cc885b2a931aa03a875a8884718c0afcc971a
vetlehaa <vetlehaa@stud.ntnu.no>
2024-03-21 12:03:01 +0100

    Added .spo

---
+++ sim/MILESTONE3/DTT_obj_dir/outputs.h
@@ -0,0 +1,3 @@
+/* Generated code: do not edit. */
+VL_DATA(8,rst_cap,0,0)
+VL_DATA(8,register_out,7,0)
---
+++ sim/MILESTONE3/DTT_obj_dir/verilator_shim.d
@@ -0,0 +1,7 @@
+verilator_shim.o: /opt/eda/share/ngspice/scripts/src/verilator_shim.cpp \
+ /usr/share/verilator/include/verilated.h \
+ /usr/share/verilator/include/verilatedos.h Vlng.h \
+ /opt/eda/share/ngspice/scripts/src/ngspice/cmtypes.h \
+ /opt/eda/share/ngspice/scripts/src/ngspice/miftypes.h \
+ /opt/eda/share/ngspice/scripts/src/ngspice/cosim.h outputs.h inouts.h \
+ inputs.h
