a590db066f4b5aac8162ae32d991851a6cef25f0
Ocheid <81308819+Ocheid@users.noreply.github.com>
2024-04-19 11:09:17 +0200

    Layout tested with verilator

--- sim/MILESTONE1/tran.py
+++ sim/MILESTONE1/tran.py
@@ -4,15 +4,19 @@ import yaml
 
 def main(name):
   # Delete next line if you want to use python post processing
-  return
+  #return
   yamlfile = name + ".yaml"
+
+
 
   # Read result yaml file
   with open(yamlfile) as fi:
     obj = yaml.safe_load(fi)
 
-  # Do something to parameters
 
+  print("iout")
+  # Do something to parameters
+  print(obj)
   # Save new yaml file
   with open(yamlfile,"w") as fo:
     yaml.dump(obj,fo)
