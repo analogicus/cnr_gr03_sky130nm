ecd7930e6b519ade37fe973562973f454beb995d
Ocheid <81308819+Ocheid@users.noreply.github.com>
2024-04-21 12:40:28 +0200

    blabla

--- sim/MILESTONE1/tran.py
+++ sim/MILESTONE1/tran.py
@@ -1,19 +1,27 @@
 #!/usr/bin/env python3
 import pandas as pd
 import yaml
+import matplotlib.pyplot as plt
+import numpy as np
 
 def main(name):
   # Delete next line if you want to use python post processing
   #return
   yamlfile = name + ".yaml"
+  rawfile = name + ".raw"
 
-
+  with open(rawfile, "rb") as f:
+    raw_data = f.read()
 
   # Read result yaml file
   with open(yamlfile) as fi:
     obj = yaml.safe_load(fi)
 
 
+  #data = np.frombuffer(raw_data, dtype=np.float32)
+
+  data = raw_data[2]
+  print(data)
   print("iout")
   # Do something to parameters
   print(obj)
